653926666d123b100b80e398f5c7cc11c516062d
maenard <maenardaboabo065@gmail.com>
2024-07-29 02:40:10 +0800

    added background configured picom

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -43,12 +43,8 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-      flavour = "mocha", -- latte, frappe, macchiato, mocha
-      background = { -- :h background
-        light = "latte",
-        dark = "mocha",
-      },
-      transparent_background = false, -- disables setting the background color.
+      flavour = "auto", -- latte, frappe, macchiato, mocha
+      transparent_background = true, -- disables setting the background color.
       show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
       term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
       dim_inactive = {
